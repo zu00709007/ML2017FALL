5e8ffc88de1efce4085fa3ceed68dc9e3d04ca8d
JiYen <s0917170207@gmail.com>
2017-10-01 23:46:10 +0800

    modified:   hw1/hw1.py

--- hw1/hw1.py
+++ hw1/hw1.py
@@ -4,31 +4,32 @@ import codecs
 import pandas
 
 training_time = 10
-learning_rate = 0.2
+learning_rate = 0.01
 w = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 total = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 x_item = list()
 y_item = list()
 
-# read process and split the data into x_vector and y_vector(PM2.5)
+# read process and split the data into x_vector and y_vector(PM2.5) and add constant
 for i in range(3,27):
 	col = pandas.read_csv(r"train.csv",usecols=[i]).values.tolist()
-	for i in range(0, len(col), 18):
-		if 'NR' == ''.join(col[i+10]):
-			col[i + 10].clear()
-			col[i + 10].append('0')
-		x_item.append(col[i : i + 9] + col[i + 10: i + 18] + [['1']])
-		y_item.append(col[i + 9: i + 10][0][0])
+	for j in range(0, len(col), 18):
+		if 'NR' == ''.join(col[j+10]):
+			col[j + 10].clear()
+			col[j + 10].append('0')
+		x_item.append(col[j : j + 9] + col[j + 10: j + 18] + [['1']])
+		y_item.append(col[j + 9: j + 10][0][0])
 		
 # start training w
 data_len = len(y_item)
 for i in range(0,training_time):
+	total = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 	for j in range(0,data_len):
-		# get wx
+		# get wx by using inner product
 		inner_product = 0
-		total = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 		for k in range(0,18):
 			inner_product = inner_product + w[k] * float(x_item[j][k][0])
+			
 		# get y - wx
 		inner_product = float(y_item[j]) - inner_product
 		
@@ -36,12 +37,10 @@ for i in range(0,training_time):
 		for k in range(0,17):
 			total[k] = total[k] + learning_rate * inner_product * float(x_item[j][k][0])
 			
-		# Constant term
+		# constant
 		total[17] = total[17] + learning_rate * inner_product
 		
-	#renew here w
+	# update w
 	for k in range(0,18):
 		w[k] = w[k] + total[k] / data_len
-
-for k in range(0,18):	
-	print(w[k])
+		
