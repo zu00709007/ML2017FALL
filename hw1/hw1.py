e6b61c75ffcb2333fa75ff4864bd2e5f5a0bdb58
JiYen <s0917170207@gmail.com>
2017-10-02 21:46:28 +0800

    modified:   hw1/hw1.py

--- hw1/hw1.py
+++ hw1/hw1.py
@@ -3,8 +3,8 @@ import sys
 import codecs
 import pandas
 
-training_time = 10
-learning_rate = 0.01
+training_time = 1000
+learning_rate = 0.0001
 w = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 total = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 x_item = list()
@@ -24,14 +24,18 @@ for i in range(3,27):
 data_len = len(y_item)
 for i in range(0,training_time):
 	total = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+	cost= 0
 	for j in range(0,data_len):
 		# get wx by using inner product
 		inner_product = 0
 		for k in range(0,18):
 			inner_product = inner_product + w[k] * float(x_item[j][k][0])
-			
+		
 		# get y - wx
-		inner_product = float(y_item[j]) - inner_product
+		inner_product = inner_product - float(y_item[j])
+		
+		# get total cost
+		cost = cost + inner_product * inner_product
 		
 		# get Σ(y - wx)x 
 		for k in range(0,17):
@@ -40,7 +44,10 @@ for i in range(0,training_time):
 		# constant
 		total[17] = total[17] + learning_rate * inner_product
 		
+	
 	# update w
 	for k in range(0,18):
-		w[k] = w[k] + total[k] / data_len
+		w[k] = w[k] - total[k] / data_len
 		
+print(cost/(2*data_len))
+#print(w)	
