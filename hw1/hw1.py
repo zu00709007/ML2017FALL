e4ed514a7a15d21513cfc0fb7ca2dabec373e5c8
JiYen <s0917170207@gmail.com>
2017-10-03 17:45:15 +0800

    new file:   hw1/.~lock.test.csv# 	modified:   hw1/hw1.py 	new file:   hw1/output.csv 	copied:     hw1/hw1.py -> hw1/train.py

--- hw1/hw1.py
+++ hw1/hw1.py
@@ -3,44 +3,30 @@ import sys
 import codecs
 import pandas
 
-training_time = 100000
-learning_rate = 0.00002
-w = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-total = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+w = [1] * 18
 x_item = list()
-y_item = list()
-
-# read process and split the data into x_vector and y_vector(PM2.5) and add constant
-for i in range(3,27):
-	col = pandas.read_csv(r"train.csv",usecols=[i]).values.tolist()
+y_item = [0] * 240
+# read process and split the data into x_vector
+for i in range(8,11):
+	col = pandas.read_csv(r"test.csv", header=-1, usecols=[i]).values.tolist()
 	for j in range(0, len(col), 18):
 		if 'NR' == ''.join(col[j+10]):
 			col[j + 10].clear()
 			col[j + 10].append('0')
-		x_item.append(col[j : j + 9] + col[j + 10: j + 18] + [['1']])
-		y_item.append(col[j + 9: j + 10][0][0])
+		x_item.append(col[j : j + 9] + col[j + 10: j + 18])
 		
-# start training w
-data_len = len(y_item)
-for i in range(0,training_time):
-	total = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-	cost= 0
-	for j in range(0,data_len):
-		# get wx by using inner product
-		inner_product = 0
-		for k in range(0,18):
-			inner_product = inner_product + w[k] * float(x_item[j][k][0])		
-		# get y - wx
-		inner_product = inner_product - float(y_item[j])		
-		# get total cost
-		cost = cost + inner_product * inner_product		
-		# get Σ(y - wx)x 
-		for k in range(0,17):
-			total[k] = total[k] + learning_rate * inner_product * float(x_item[j][k][0])			
-		# constant
-		total[17] = total[17] + learning_rate * inner_product	
-	# update w
-	for k in range(0,18):
-		w[k] = w[k] - total[k] / data_len		
-print(cost/(2*data_len))
-#print(w)	
+# start testing x_vector to get y_vector
+data_len = len(x_item)
+for i in range(0,data_len):
+	# add bias
+	inner_product = w[17]
+	for j in range(0,17):
+		inner_product += w[j] * float(x_item[i][j][0])
+	y_item[i%240] += inner_product
+	
+
+with open('output.csv', 'w', encoding='utf-8') as f:
+	spamwriter = csv.writer(f, delimiter=',')
+	spamwriter.writerow(['id', 'value'])
+	for i in range(0, 240):
+		spamwriter.writerow(['id_'+str(i), y_item[i]/3])
---
+++ hw1/train.py
@@ -0,0 +1,48 @@
+import csv
+import sys
+import codecs
+import pandas
+
+training_time = 1000000
+# max learning_rate is 0.00002
+learning_rate = 0.00001
+w = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+total = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+x_item = list()
+y_item = list()
+
+# read process and split the data into x_vector and y_vector(PM2.5) and add constant
+for i in range(3,27):
+	col = pandas.read_csv(r"train.csv",usecols=[i]).values.tolist()
+	for j in range(0, len(col), 18):
+		if 'NR' == ''.join(col[j+10]):
+			col[j + 10].clear()
+			col[j + 10].append('0')
+		x_item.append(col[j : j + 9] + col[j + 10: j + 18] + [['1']])
+		y_item.append(col[j + 9: j + 10][0][0])
+		
+# start training w
+data_len = len(y_item)
+for i in range(0,training_time):
+	total = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+	cost= 0
+	for j in range(0,data_len):
+		# get wx by using inner product
+		inner_product = 0
+		for k in range(0,18):
+			inner_product += w[k] * float(x_item[j][k][0])		
+		# get y - wx
+		inner_product -= float(y_item[j])		
+		# get total cost
+		cost = cost + inner_product ** 2		
+		# get Σ(y - wx)x 
+		for k in range(0,17):
+			total[k] += learning_rate * inner_product * float(x_item[j][k][0])			
+		# constant
+		total[17] += learning_rate * inner_product	
+	# update w
+	for k in range(0,18):
+		w[k] = w[k] - total[k] / data_len	
+		
+print(cost/(2*data_len))
+print(w)	
